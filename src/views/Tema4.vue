ff6ee2ee7176589ccf6732ffa90eebe22a6eac2f
Camilo Torres <83730668+DevJohn2021@users.noreply.github.com>
2021-05-11 17:35:15 -0500

    listo tema4

--- src/views/Tema4.vue
+++ src/views/Tema4.vue
@@ -7,7 +7,40 @@
       .titulo-principal__numero
         .h3 4
       .h3 Negociación digital (modelo de negociación)
-    p.mb-4
+
+    p.mb-4 Un modelo de negocio articula la lógica, los datos y otras pruebas que respaldan una propuesta de valor para el cliente y una estructura viable de ingresos y costos para la empresa que proporciona ese valor. Se trata del beneficio que la empresa ofrecerá a los clientes, cómo se organizará para hacerlo y cómo capturará una parte del valor que entrega. Un buen modelo de negocio tiene un valor considerable para el cliente y recogerá (para el desarrollador o ejecutor del modelo de negocio) una parte viable de este valor.
+
+    .row.mb-4
+      .col-lg-7
+        .cajon--azul.p-4
+          p.mb-3 Pero desarrollar un modelo de negocio exitoso, por muy novedoso que sea, no es suficiente en sí mismo para garantizar una ventaja competitiva. Una vez implantados, los elementos de los modelos de negocio suelen ser bastante transparentes y en principio fáciles de imitar; de hecho, suele ser cuestión de unos pocos años, si no de meses, que un nuevo modelo de negocio
+          p En los tiempos de Covid-19 ha crecido el uso de la tecnología y se ha vuelto una necesidad indispensable a nivel empresarial para su sostenibilidad en las relaciones comerciales. La empresa de hoy debe adaptarse a las nuevas tecnologías para mantenerse en el mercado. Las maneras de compra y venta tradicionales para las negociaciones se han modificado debido al mayor uso de las herramientas informáticas. La confianza en las transacciones son servicios del e-commerce que se encuentran como valor agregado a la disminución de costos, ocasionan mayor manufactura, agilidad y calidad en un menor tiempo, lo que la hace más competitiva
+      .col-lg-5.align-self-center
+        figure
+          img(src='@/assets/curso/img17.svg', alt='Modelo de negociación')
+
+    p.mb-4 En la práctica, los modelos de negocio de éxito suelen ser, en cierta medida, "compartidos" por múltiples competidores. Se conocen diferentes maneras de hacer e-commerce, primeramente, describimos las más usadas:
+
+    .row.mb-4
+      .col-lg-7.align-self-center
+        figure
+          img(src='@/assets/curso/img18.svg', alt='Modelos de negocip')
+      .col-lg-5
+        p #[strong B2B o business to business:] la transacción se realiza entre empresas que operan en internet. 
+        p #[strong B2C o business to costumer:] es el comercio entre la empresa que produce, la que vende o prestadora de servicios y el consumidor final. 
+        p #[strong B2E o business to employee:] los negocios se realizan entre la empresa y sus colaboradores con beneficios exclusivos. 
+        p #[strong C2C o costumer to costumer:] transacción en la que el cliente adquiere un producto o servicio y realiza la acción de reventa.
+        p #[strong G2C o government to costumeer:] la transacción es de gobierno a consumidor por ejemplo pagos de impuestos o multas vehiculares.
+        p #[strong G2B o government to business:] negocio entre el gobierno y las empresas como los portales de compras y licitaciones.
+
+    .row.mb-4
+      .col-lg-6.align-self-center
+        p.mb-4 A razón de la utilización de la tecnología, la forma de comercializar ha experimentado buenos cambios generacionales, en la actualidad la incursión en el e-commerce permite aprovechar los beneficios que ofrece la globalización (Meltzer, 2018). La importancia del e-commerce tiene un marcado crecimiento a través de los años por lo que, actualmente, comercializar productos a través de esta modalidad representa un paso adelante respecto a la competencia.
+        p Debido al uso de las herramientas tecnológicas, la manera de vender ha presentado cambios generacionales, hoy por hoy la incursión del comercio electrónico permite el aprovechamiento que ofrece la globalización ampliando mercados.
+      .col-lg-6.align-self-center
+        figure
+          img(src='@/assets/curso/img19.svg', alt='Modelos de negocio')
+
 </template>
 <script>
 export default {
