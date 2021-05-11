fc7b542a51859954935d4109ef49d70fd622cb98
Camilo Torres <83730668+DevJohn2021@users.noreply.github.com>
2021-05-11 16:49:33 -0500

    tema tres listo

--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -3,23 +3,98 @@
   BannerInterno
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
     
-    #t_3_1.pb-5.h2 tema 3.1
-    .pb-5(v-for="item of 15")
+    .titulo-principal
+      .titulo-principal__numero
+        .h3 3
+      .h3 Normativa e-commerce (normas y leyes para modelo e-commerce)
+    
+    p.mb-5 En Colombia, la entidad privada Cámara Colombiana de Comercio Electrónico – CCCE, es la comisionada para regular el comercio electrónico y tiene como fin promover y fortalecer la industria e-commerce mediante la recolección de cifras que precisan su conducta en el país, y repercusión de políticas públicas y la promoción del conocimiento y seguridad del sector.
+    
+    .row.justify-content-center.align-items-center.mb-5.bloque-custom-a
+      .col-lg-5.bloque-custom-b.p-5
+        .row.justify-content-end
+          .col-12.p-0
+            p.p-4 Debido a la constante labor de relacionamiento entre la CCCE, diversas entidades gubernamentales y empresas privadas, en 2017 se crea el observatorio eCommerce, una iniciativa del Ministerio de Tecnologías de la Información y las Comunicaciones – MinTIC, la Red Nacional Académica de Tecnología Avanzada – RENATA y la CCCE, cuya principal función es caracterizar y monitorear el ecosistema del comercio electrónico en Colombia, con el fin de generar conocimiento y apoyar la toma de decisiones públicas y privadas que promuevan la consolidación de la industria.
+      .col-lg-7.bloque-custom-a.p-4
+        .row.justify-content-center
+          .col-12.bloque-custom
+            p.p-4 A lo largo de sus dos años de labores, el observatorio eCommerce ha publicado estudios como el estudio de oferta y demanda del comercio electrónico en Colombia 2018, el cuarto estudio de transacciones digitales 2016-2017, el estudio del marco regulatorio del eCommerce en los países de la OECD y Colombia – objetivos y alcance, el manual de buenas prácticas para pasarelas de pago, el estudio de transacciones digitales 2018, entre otros.
+            p.p-4 Paralelamente, en 2017 la CCCE convencida de su rol de servicio como dinamizadora entre los diversos participantes del sector, creó el eCommerce Summit, un congreso internacional de comercio electrónico que otorga espacios de empoderamiento, diálogos y conocimiento entre los principales actores del comercio electrónico.
+      
+    p.mb-4 Actualmente, la Cámara Colombiana de Comercio Electrónico cuenta cuatro gerencias (administrativa y financiera, políticas públicas y asuntos legales, análisis económico, y marketing y gestión comercial) los cuales dan respuesta a las necesidades del sector, siempre en línea con los tres pilares fundamentales de la organización:
 
-    #t_3_2.pb-5.h2 tema 3.2
-    .pb-5(v-for="item of 15")
+    .row.justify-content-end.align-items-center.mb-4
+      .col-12.col-lg-11
+         ul.fa-ul
+              li.mb-4 #[span.li--clr.fa-li #[i.fas.fa-chevron-right]] Entendimiento de las dinámicas económicas que determinan al sector e-commerce del país.
+              li.mb-4 #[span.li--clr.fa-li #[i.fas.fa-chevron-right]] Contribución a la construcción de políticas públicas que favorezcan a la industria e-commerce.
+              li.mb-4 #[span.li--clr.fa-li #[i.fas.fa-chevron-right]] Generación de conocimiento y confianza hacia el sector.
+    
+    .row.mx-auto.mb-5
+      .cajon--rojo.p-4
+        p.px-5 Al cierre del 2019, la CCCE cuenta con alrededor de 300 empresas afiliadas, de las cuales el 53 % corresponde a proveedores tecnológicos y el 47 % a tiendas online, además de trabajar en iniciativas como el sello de confianza para pasarelas de pago y diversas mesas de trabajo interinstitucionales, vinculando entidades como MinTIC, MinCit, ProColombia, entre otras.
 
-    #t_3_3.pb-5.h2 tema 3.3
-    .pb-5(v-for="item of 15")
+    
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-11
+        a.anexo.mb-4.mb-lg-0(href="https://www.ccce.org.co/preguntas-frecuentes/#1576774113499-57f66893-8bbf" target="_blank")
+            .anexo__icono
+              img(src="@/assets/template/icono-link.svg")
+            .anexo__texto
+              p #[strong Enlace web.] Para mayor información sobre requisitos funciones, vigilancia, control, actividades y consultoría pueden dirigirse al enlace, donde encontrarán la regulación vigente en el sector e-commerce  en Colombia.
 
-    #t_3_4.pb-5.h2 tema 3.4
-    .pb-5(v-for="item of 15")
+    .row.justify-content-end.align-items-center.mb-5
+      .col-lg-11
+        .h5 Por otra parte, la Constitución Política contempla protección para e-commerce en los siguientes artículos: (Constitución Política de Colombia, 1991):
 
-    #t_3_5.pb-5.h2 tema 3.5
-    .pb-5(v-for="item of 15")
+    .row
+      .col-lg-12
+        .tarjeta-Avatar-b.mb-5
+          img(src='@/assets/curso/img14.svg' alt='AvatarTop')
+          .tarjeta.tarjeta--azul.p-4
+            p #[stron Articulo 15:] todas las personas tienen derecho a su intimidad personal y familiar y a su buen nombre, y el Estado debe respetarlos y hacerlos respetar. De igual modo, tienen derecho a conocer, actualizar y rectificar las informaciones que se hayan recogido sobre ellas en bancos de datos y en archivos de entidades públicas y privadas. En la recolección, tratamiento y circulación de datos se respetarán la libertad y demás garantías consagradas en la Constitución. La correspondencia y demás formas de comunicación privada son inviolables. Sólo pueden ser interceptadas o registradas mediante orden judicial, en los casos y con las formalidades que establezca la ley. Para efectos tributarios o judiciales y para los casos de inspección, vigilancia e intervención del Estado podrá exigirse la presentación de libros de contabilidad y demás documentos privados, en los términos que señale la ley. Lo anterior es conocido en el medio legal como Habeas Data.
+    
+    .row
+      .col-lg-12
+        .tarjeta-Avatar-b.mb-5
+          img(src='@/assets/curso/img14.svg' alt='AvatarTop')
+          .tarjeta.tarjeta--azul.p-4
+            p #[stron Articulo 20:] se garantiza a toda persona la libertad de expresar y difundir su pensamiento y opiniones, la de informar y recibir información veraz e imparcial, y la de fundar medios masivos de comunicación. Estos son libres y tienen responsabilidad social. Se garantiza el derecho a la rectificación en condiciones de equidad. No habrá censura. Lo anterior resume la protección a la libertad de expresión, que constituye una de las principales garantías para el e-commerce.
 
-    #t_3_6.pb-5.h2 tema 3.6
-    .pb-5(v-for="item of 15")
+    .row
+      .col-lg-12
+        .tarjeta-Avatar-b.mb-5
+          img(src='@/assets/curso/img14.svg' alt='AvatarTop')
+          .tarjeta.tarjeta--azul.p-4
+            p #[stron Articulo 333:] la actividad económica y la iniciativa privada son libres, dentro de los límites del bien común. Para su ejercicio, nadie podrá exigir permisos previos ni requisitos, sin autorización de la ley. La libre competencia económica es un derecho de todos que supone responsabilidades
+
+    P.mb-4 Además de lo anterior, los modelos nuevos de hacer negocios por medios electrónicos presentan como base la ley 527 de 1999 y el decreto 1074 de 2015, que contemplan normas del sector comercio y turismo y el estatuto del consumidor a partir de la ley 1480 de 2011, el cual establece en su capítulo VI, protección especial a los consumidores de comercio electrónico, los cuales  permiten su funcionamiento pero sobre todo que validan y otorgan eficacia legal a la creciente actividad desmaterializada. La contratación electrónica encuentra un respaldo en su formación y ejecución. La oferta y aceptación comercial por medios electrónicos con validez genera relaciones jurídicas relevantes. Los objetos conectados se unen mediante mensajes de datos. Los algoritmos de los contratos inteligentes desarrollan la contratación electrónica con ejecución automatizada. Los asistentes virtuales interactúan con otros sistemas de información o con los seres humanos a los que apoyan mediante mensajes de datos.
+    p.mb-5 La siguiente imagen nos presenta un esquema de la Ley 527 de 1991.
+
+    figure.mb-5
+      img(src='@/assets/curso/img15.svg', alt='Ley 527 de 1991, SENA 2021')
+
+    .row
+      .col-lg-7
+        p.mb-4 Otra normativa es el decreto 1747 de 2000, por medio del cual se define y reglamenta el acceso y uso de los mensajes de datos, del comercio electrónico y de las firmas digitales, y se establecen las entidades de certificación y se dictan otras disposiciones. Y que relaciona ampliamente las entidades de certificación y certificados digitales, art. 2 al 25. Facultades de la Superintendencia de Industria y Comercio Art 26 a 28.
+        p.mb-4 Uno de los aspectos importantes en la normativa e-commerce es la protección de datos personales, la cual se debe incluir en las transacciones mediante el derecho de hábeas data y se rige por las siguientes leyes:
+
+        a.anexo.mb-4.mb-lg-0(href="https://www.ccce.org.co/preguntas-frecuentes/#1576774113499-57f66893-8bbf" target="_blank")
+          .anexo__icono
+            img(src="@/assets/template/icono-link.svg")
+          .anexo__texto           
+            p #[strong Enlace web.] Se exponen las disposiciones generales del habeas data y se regula el manejo de la información contenidas en las bases de datos personales, especial la financiera, crediticia, comercial, de servicios y la proveniente de terceros países y se dictan nuevas disposiciones.
+        br
+
+        a.anexo.mb-4.mb-lg-0(href="https://www.ccce.org.co/preguntas-frecuentes/#1576774113499-57f66893-8bbf" target="_blank")
+          .anexo__icono
+            img(src="@/assets/template/icono-link.svg")
+          .anexo__texto
+            p #[strong Enlace web.] Desarrolla el objeto constitucional que tienen todas las personas a conocer, actualizar y rectificar las informaciones que se hayan recogido sobre ellas en bases de datos, archivos,  libertades y garantías constitucionales a que se refiere el artículo 15 de la Constitución Política.
+      
+      .col-lg-5.align-self-center
+        figure
+          img(src='@/assets/curso/img16.svg', alt='Decreto 1747 de 2000, SENA 2021')
 
 </template>
 
---
+++ src/views/Tema4.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+.curso-main-container.pb-3
+  BannerInterno
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5
+    
+    .titulo-principal
+      .titulo-principal__numero
+        .h3 4
+      .h3 Negociación digital (modelo de negociación)
+    p.mb-4
+</template>
+<script>
+export default {
+  name: 'Tema4',
+  data: () => ({
+    // variables de vue
+  }),
+}
+</script>
+
+<style lang="sass" scoped></style>
